cd50b1c7ae7b44ad00e1794aec1e80841729eba6
EC2 Default User <ec2-user@ip-172-31-32-136.ec2.internal>
2021-06-17 15:19:31 +0000

    Demo

--- eks_cdk/webservice_stage.py
+++ eks_cdk/webservice_stage.py
@@ -1,6 +1,6 @@
 from aws_cdk import core
 
-from .cdk_eks_1_stack import CdkeksStack
+from .eks_cdk_stack import CdkeksStack
 
 class WebServiceStage(core.Stage):
   def __init__(self, scope: core.Construct, id: str, **kwargs):
